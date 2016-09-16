fc980a85643f6ca03c4cce935b45fd04d819ca4c
Alexander Murphy <amurphy345@gmail.com>
2016-09-16 10:47:26 -0600

    word cloud working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,37 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app works!';
+  private _textBlob: string
+  public hasWordCloud: boolean = false   
+  public completedWordMap: any = []
+
+  public generateWordCloud(text) {
+    this._textBlob = text;
+    this.generateHashMap()
+  }
+
+  public getFontSize(count) {
+    return (count * 4).toString + "px"
+  }
+
+  private _countWord(word, blob) {
+    return blob.split(" ").filter(blobWord => blobWord === word).length
+  }
+
+  private generateHashMap() {
+    let wordMap = new Map()
+    this._textBlob.split(" ").forEach((val, key) => {
+      let wordCount =  this._countWord(val, this._textBlob)      
+      wordMap.set(val, wordCount)     
+      this.completedWordMap.push({
+        count: wordCount,
+        fsize: wordCount * 17,
+        word: val + "  "
+      })
+    })
+    this.hasWordCloud = true
+    console.log(wordMap);
+  }
 }
